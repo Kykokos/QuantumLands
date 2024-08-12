eabd0dcc5805f6e45fc82ea350a0985dd69f93cd
Kykokos <kykokospro@gmail.com>
2024-08-12 16:23:10 +0200

    Initial commit

--- src/main/java/net/Kykokos/QuantumLands/Item/custom/BunkerDetectorItem.java
+++ src/main/java/net/Kykokos/QuantumLands/Item/custom/BunkerDetectorItem.java
@@ -2,6 +2,7 @@ package net.Kykokos.QuantumLands.Item.custom;
 
 import com.mojang.authlib.minecraft.TelemetrySession;
 import net.Kykokos.QuantumLands.Block.ModBlocks;
+import net.Kykokos.QuantumLands.util.ModTags;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.core.BlockPos;
@@ -96,23 +97,7 @@ public class BunkerDetectorItem extends Item
 
     private boolean isBunkerBlock(BlockState blockState)
     {
-        return  blockState.is(Blocks.CHEST) || blockState.is(Blocks.BARREL) || blockState.is(Blocks.SHULKER_BOX) || blockState.is(ModBlocks.BUNKER_BLOCK.get())
-                || blockState.is(Blocks.GRAY_CONCRETE) || blockState.is(Blocks.BRICKS) || blockState.is(Blocks.PACKED_MUD) || blockState.is(Blocks.SMOOTH_STONE)
-                || blockState.is(Blocks.MUD_BRICKS) || blockState.is(Blocks.CYAN_CONCRETE) || blockState.is(Blocks.BLACK_CONCRETE) || blockState.is(Blocks.BLUE_CONCRETE)
-                || blockState.is(Blocks.BROWN_CONCRETE) || blockState.is(Blocks.GREEN_CONCRETE) || blockState.is(Blocks.LIGHT_BLUE_CONCRETE) || blockState.is(Blocks.LIGHT_GRAY_CONCRETE)
-                || blockState.is(Blocks.LIME_CONCRETE) || blockState.is(Blocks.MAGENTA_CONCRETE) || blockState.is(Blocks.PINK_CONCRETE) || blockState.is(Blocks.RED_CONCRETE)
-                || blockState.is(Blocks.ORANGE_CONCRETE) || blockState.is(Blocks.PURPLE_CONCRETE) || blockState.is(Blocks.WHITE_CONCRETE) || blockState.is(Blocks.YELLOW_CONCRETE)
-                || blockState.is(Blocks.SMOOTH_STONE_SLAB) || blockState.is(Blocks.QUARTZ_BLOCK) || blockState.is(Blocks.SMOOTH_QUARTZ) || blockState.is(Blocks.STONE_BRICKS)
-                || blockState.is(Blocks.STONE_BRICK_SLAB)|| blockState.is(Blocks.CRACKED_STONE_BRICKS) || blockState.is(Blocks.STONE_BRICKS) || blockState.is(Blocks.DEEPSLATE_BRICKS)
-                || blockState.is(Blocks.DEEPSLATE_BRICK_SLAB) || blockState.is(Blocks.REDSTONE_BLOCK) || blockState.is(Blocks.COMPARATOR) || blockState.is(Blocks.REPEATER)
-                || blockState.is(Blocks.PISTON) || blockState.is(Blocks.STICKY_PISTON) || blockState.is(Blocks.SLIME_BLOCK) || blockState.is(Blocks.HONEY_BLOCK)
-                || blockState.is(Blocks.OBSERVER) || blockState.is(Blocks.IRON_BLOCK) || blockState.is(Blocks.REDSTONE_LAMP) || blockState.is(Blocks.FURNACE) || blockState.is(Blocks.BLAST_FURNACE)
-                || blockState.is(Blocks.SMOKER) || blockState.is(Blocks.CRAFTING_TABLE) || blockState.is(Blocks.OAK_PLANKS) || blockState.is(Blocks.OAK_SLAB)
-                || blockState.is(Blocks.DARK_OAK_PLANKS) || blockState.is(Blocks.DARK_OAK_SLAB) || blockState.is(Blocks.ACACIA_PLANKS) || blockState.is(Blocks.ACACIA_SLAB)
-                || blockState.is(Blocks.JUNGLE_PLANKS) || blockState.is(Blocks.JUNGLE_SLAB) || blockState.is(Blocks.BIRCH_PLANKS) || blockState.is(Blocks.BIRCH_SLAB)
-                || blockState.is(Blocks.SPRUCE_PLANKS) || blockState.is(Blocks.SPRUCE_SLAB) || blockState.is(Blocks.BAMBOO_PLANKS) || blockState.is(Blocks.BAMBOO_SLAB)
-                || blockState.is(Blocks.MANGROVE_PLANKS) || blockState.is(Blocks.MANGROVE_SLAB) || blockState.is(Blocks.CHERRY_PLANKS) || blockState.is(Blocks.CHERRY_SLAB)
-                || blockState.is(Blocks.WARPED_PLANKS) || blockState.is(Blocks.WARPED_SLAB) || blockState.is(Blocks.CRIMSON_PLANKS) || blockState.is(Blocks.CRIMSON_SLAB)
-                || blockState.is(Blocks.BAMBOO_MOSAIC_SLAB) || blockState.is(Blocks.BAMBOO_MOSAIC) || blockState.is(Blocks.OBSIDIAN);
+        return  blockState.is(ModTags.Blocks.BUNKER_DETECTOR_TARGETS);
+
     }
 }
