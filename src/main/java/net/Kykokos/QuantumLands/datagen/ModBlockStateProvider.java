78cdda701ad60ae7b2ccf21c64b97ce0bd55a0a6
Kykokos <kykokospro@gmail.com>
2024-09-28 23:50:27 +0200

    Initial commit

--- src/main/java/net/Kykokos/QuantumLands/datagen/ModBlockStateProvider.java
+++ src/main/java/net/Kykokos/QuantumLands/datagen/ModBlockStateProvider.java
@@ -1,6 +1,33 @@
 package net.Kykokos.QuantumLands.datagen;
 
-public class ModBlockStateProvider
+import net.Kykokos.QuantumLands.Block.ModBlocks;
+import net.Kykokos.QuantumLands.QuantumLands;
+import net.minecraft.data.PackOutput;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModBlockStateProvider extends BlockStateProvider
 {
 
+    public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
+        super(output, QuantumLands.MOD_ID, exFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels()
+    {
+        blockWithItem(ModBlocks.BUNKER_BLOCK);
+        blockWithItem(ModBlocks.CRACKED_BUNKER_BLOCK);
+        blockWithItem(ModBlocks.SNIFFERITE_ORE);
+        blockWithItem(ModBlocks.DEEPSLATE_SNIFFERITE_ORE);
+    }
+
+    private void blockWithItem(RegistryObject<Block> blockRegistryObject)
+    {
+        simpleBlockWithItem(blockRegistryObject.get(), cubeAll(blockRegistryObject.get()));
+    }
 }
+
+
