9ecde12c29b2ca4974b210ff76b2d8322accb860
Kykokos <kykokospro@gmail.com>
2024-09-28 23:40:34 +0200

    Initial commit

--- src/main/java/net/Kykokos/QuantumLands/datagen/ModItemModelProvider.java
+++ src/main/java/net/Kykokos/QuantumLands/datagen/ModItemModelProvider.java
@@ -1,4 +1,36 @@
 package net.Kykokos.QuantumLands.datagen;
 
-public class ModItemModelProvider {
+import net.Kykokos.QuantumLands.Item.ModItems;
+import net.Kykokos.QuantumLands.QuantumLands;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.client.model.generators.ItemModelBuilder;
+import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
+
+
+public class ModItemModelProvider extends ItemModelProvider {
+
+    public ModItemModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
+        super(output, QuantumLands.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void registerModels() {
+        simpleItem(ModItems.SNIFFERITE);
+        simpleItem(ModItems.SNIFFERITE_DUST);
+        simpleItem(ModItems.BLACKOUT_POWDER);
+        simpleItem(ModItems.BUKER_DETECTOR);
+        simpleItem(ModItems.PEAT_BRICK);
+    }
+
+    private ItemModelBuilder simpleItem(RegistryObject<Item> item)
+    {
+        return withExistingParent(item.getId().getPath(),
+                new ResourceLocation("item/generated")).texture("layer0",
+                new ResourceLocation(QuantumLands.MOD_ID, "item/" + item.getId().getPath()));
+    }
 }
